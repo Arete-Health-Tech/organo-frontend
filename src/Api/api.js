6dbdae06fddcd0b105c9d409afc1f649516a3096
mr-clover001 <anoop@mrclover.local>
2025-01-10 17:03:22 +0530

    change in api

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,30 +1,29 @@
 import axios from "axios";
 
 // const baseUrl = 'http://localhost:3009/api/backend/endpoint/'
-const baseUrl = 'https://organo.aretehealth.tech/'
-
+const baseUrl = "https://organo.aretehealth.tech/api/backend/endpoint";
 
 // Define the async function that fetches the data
 const fetchOrganoApi = async (payload) => {
-    try {
-        const response = await axios.post(`${baseUrl}getAuditComments`, payload);
-        return response.data;  // or return the full response if needed
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        throw error;  // Optionally handle the error
-    }
+  try {
+    const response = await axios.post(`${baseUrl}getAuditComments`, payload);
+    return response.data; // or return the full response if needed
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Optionally handle the error
+  }
 };
 
 export const searchOrganoApi = async (searchEmployeeId) => {
-    try {
-        const response = await axios.post(`${baseUrl}/search`, {
-            employeeId: searchEmployeeId,
-        });
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        throw error;
-    }
+  try {
+    const response = await axios.post(`${baseUrl}/search`, {
+      employeeId: searchEmployeeId,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
 
 // Export the async function for use in other parts of your app
